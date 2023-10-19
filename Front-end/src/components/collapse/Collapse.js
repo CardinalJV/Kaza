7abfebb51ee9e776d727a123cfe52529ca61cbb8
CardinalJV <jessy.viranaiken@icloud.com>
2023-10-19 16:59:34 +0200

    Création du composant collapse et ajout dans la page about

---
+++ Front-end/src/components/collapse/Collapse.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import './Collapse.css';
+
+const Collapse = (props) => {
+    const { title, text } = props;
+    const [isTextVisible, setIsTextVisible] = useState(false);
+
+    const toggleText = () => {
+        setIsTextVisible(!isTextVisible);
+    };
+
+    return (
+        <div className='dropdown'>
+                <div id="content-show">
+                <h2>
+                    {title}
+                </h2>
+                <span>
+                    <FontAwesomeIcon icon={faChevronDown} className={isTextVisible ? 'to-top' : 'to-bottom'} onClick={toggleText} />
+                </span>
+                </div>
+
+            <div id='content-hidden' className={isTextVisible ? 'show' : 'ofshow'}>
+                <p>
+                   {text}
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Collapse;
+
